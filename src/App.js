8e38ff6801025eb1351e9dfcc2c5336384493cb2
Michael McKenzie <mikemckenzie987@gmail.com>
2018-11-13 15:04:50 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,28 +1,51 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+
+const githubProfile = "https://api.github.com/users/mpmckenz";
 
 class App extends Component {
+  state = { user: {}, active: false };
+
+  handleClick = event => {
+    fetch(githubProfile)
+      .then(response => response.json())
+      .then(profileInfo => {
+        this.setState(previousState => {
+          return {
+            user: profileInfo,
+            active: !previousState.active
+          };
+        });
+        // Object.entries(profileInfo);
+      });
+  };
+
+  // handleClick() {
+  //   {this.setState(prevStaeactive: !previousState.active)}
+  // }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div id="parentContainer">
+        <button onClick={this.handleClick}>View Profile</button>
+        <div id="profileContainer" />
+        <p>Profile Name: {this.state.user.name}</p>
+        {/* <img>{this.state.user.avatar_url}</img> */}
+        <p>Login: {this.state.user.login}</p>
+        <p>Number of Followers: {this.state.user.followers}</p>
+        <p>
+          {this.state.user.name} is following {` `}
+          {this.state.user.following} other users
+        </p>
       </div>
     );
   }
 }
 
 export default App;
+
+/* {Object.entries(user).map(([key, value]) => {
+      return (
+        <div>{key}: {value.toString()}</div>
+      )})
+      } */
