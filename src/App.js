346b2c2ff2dd220fce571afe726c52a2a9351296
Michael McKenzie <mikemckenzie987@gmail.com>
2018-11-14 11:42:36 -0500

    fixed card

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "./App.css";
 import { Button, Card, CardTitle, Container } from "react-materialize";
 import Typography from "@material-ui/core/Typography";
+import { CardContent } from "@material-ui/core";
 const githubProfile = "https://api.github.com/users/mpmckenz";
 
 // const testArr = { numberone: 1, numbertwo: 2, numberthree: 3 };
@@ -36,26 +37,32 @@ class App extends Component {
         </Button>
         {this.state.active ? (
           <Card
+            id="card"
             className="small"
             header={
               <div id="parent">
                 <CardTitle image={this.state.user.avatar_url}>
                   <br />
-                  <Typography variant="h3" gutterBottom>
-                    <b>{this.state.user.name}</b>
-                    <br />
-                  </Typography>
-                  <Typography variant="h4" gutterBottom>
-                    <b>Login: </b>
-                    {this.state.user.login}
-                    <br />
-                    <b>Public repositories:</b> {this.state.user.public_repos}
-                    <br />
-                    <a href="https://epic-spinners.epicmax.co/#/">
-                      Moving Icons
-                    </a>
-                  </Typography>
+                  {/* <Typography variant="h3" gutterBottom> */}
+                  <b>{this.state.user.name}</b>
+                  <br />
+                  {/* </Typography> */}
                 </CardTitle>
+                <CardContent>
+                  {/* <Typography variant="h4" gutterBottom> */}
+                  <b>Login: </b>
+                  {this.state.user.login}
+                  <br />
+                  <b>Public repositories:</b> {this.state.user.public_repos}
+                  <br />
+                  <p>
+                    Kenzie Academy is pretty cool. I really like going here and{" "}
+                    <b>REALLY want to be a student coach</b> but keeping my
+                    grade up is really hard for me to do.{" "}
+                  </p>
+                  <a href="https://epic-spinners.epicmax.co/#/">Moving Icons</a>
+                  {/* </Typography> */}
+                </CardContent>
               </div>
             }
           />
